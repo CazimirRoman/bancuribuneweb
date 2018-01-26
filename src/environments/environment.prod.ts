b92d1cd718e5e71cf79a55338dc79ad26bf4aeb0
Roman Cazimir <extern.Cazimir.Roman@elektrobit.com>
2018-01-26 15:19:53 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+      apiKey: "AIzaSyBjWpbL9e_nhZCjymleZpKp0JcBGJ2XEPA",
+      authDomain: "bancuri-bune.firebaseapp.com",
+      databaseURL: "https://bancuri-bune.firebaseio.com",
+      projectId: "bancuri-bune",
+      storageBucket: "bancuri-bune.appspot.com",
+      messagingSenderId: "973289034890"
+    }
 };
